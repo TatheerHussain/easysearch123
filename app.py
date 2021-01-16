a94f89f15925a28214723bfcc70bccbf63ff8738
tatheerhussain <tatheerhussain@outlook.com>
2021-01-17 06:37:39 +0800

    make it bettr

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def callback():
     return '',200
 
 def sendText(user, text):
-  LINE_API = 'https://developers.line.biz/console/channel/1655581941'
+  #LINE_API = 'https://developers.line.biz/console/channel/1655581941'
   headers = {
     'Content-Type': 'application/json; charset=UTF-8',
     'Authorization': os.environ['Authorization']    # ตั้ง Config vars ใน heroku พร้อมค่า Access token
